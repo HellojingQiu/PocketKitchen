d6c17da36241404a75da115550ccd53cf9882633
李超 <richou@richoutekiMacBook-Pro.local>
2014-02-16 14:20:51 +0800

    Initial Commit

---
+++ PocketKitchenGod/LLCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LLCAppDelegate.h
+//  PocketKitchenGod
+//
+//  Created by 李超 on 14-2-16.
+//  Copyright (c) 2014年 李超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PocketKitchenGod/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PocketKitchenGod
+//
+//  Created by 李超 on 14-2-16.
+//  Copyright (c) 2014年 李超. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LLCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([LLCAppDelegate class]));
+  }
+}
